29b823760ef9211057472f35c6530e44e2d08af4
GorkemCinar389 <grkmcnr1457@gmail.com>
2025-02-08 20:58:21 +0300

    Add blazuorOffcanvas object for Bootstrap offcanvas control
    Introduced blazuorOffcanvas object in blazuorOffcanvasJsInterop.js.
    This object includes two methods: `show` and `hide`.
    - `show(id)`: Initializes and displays the Bootstrap offcanvas.
    - `hide(id)`: Hides the existing Bootstrap offcanvas if it exists.

---
+++ Blazuor/wwwroot/js/Offcanvas/blazuorOffcanvasJsInterop.js
@@ -0,0 +1,12 @@
+﻿window.blazuorOffcanvas = {
+    show: (id) => {
+        let offcanvas = new bootstrap.Offcanvas(document.getElementById(id));
+        offcanvas.show();
+    },
+    hide: (id) => {
+        let offcanvas = bootstrap.Offcanvas.getInstance(document.getElementById(id));
+        if (offcanvas) {
+            offcanvas.hide();
+        }
+    }
+};
